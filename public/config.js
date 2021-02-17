20faf97a7c9aac7438925120b0156030981b0bbd
Cclss <53106015+Cclss@users.noreply.github.com>
2021-02-17 18:08:16 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -10,10 +10,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'u1198038-fd24ba3efd2e9dea65cf584a',
   ],
 
   // 是否显示监测站点的链接
